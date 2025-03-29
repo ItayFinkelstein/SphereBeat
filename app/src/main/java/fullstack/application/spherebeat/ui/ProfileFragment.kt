7cbee05b5316aeb3dd700306e15803ae465f95c1
Gil <gilgil.abramovich@gmail.com>
2025-03-29 20:27:48 +0300

    Profile- get details from auth

--- app/src/main/java/fullstack/application/spherebeat/ui/ProfileFragment.kt
+++ app/src/main/java/fullstack/application/spherebeat/ui/ProfileFragment.kt
@@ -5,10 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.google.firebase.auth.FirebaseAuth
 import fullstack.application.spherebeat.databinding.FragmentProfileBinding
+import fullstack.application.spherebeat.ui.viewModel.UserViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
@@ -18,25 +19,29 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private val currentUser = FirebaseAuth.getInstance().currentUser
+    private val userViewModel: UserViewModel by viewModels()
+
+
 
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        binding.Email.text = "Email: " + currentUser?.email
+
+        userViewModel.userList.observe(viewLifecycleOwner, { users ->
+            users?.let {
+                val selectedUser = users.find {
+                    it.email == currentUser?.email
+                }
+                binding.username.text = "Username: " + selectedUser?.name
+            }
+        })
         return binding.root
     }
 
