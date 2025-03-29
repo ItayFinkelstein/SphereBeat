287158ec8acf8cd74f5ea9b029016cd81b4a3f51
Gil <gilgil.abramovich@gmail.com>
2025-03-29 11:26:21 +0300

    SearchFragment search bar works

--- app/src/main/java/fullstack/application/spherebeat/ui/SearchFragment.kt
+++ app/src/main/java/fullstack/application/spherebeat/ui/SearchFragment.kt
@@ -1,11 +1,18 @@
 package fullstack.application.spherebeat.ui
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import fullstack.application.spherebeat.R
+import fullstack.application.spherebeat.databinding.FragmentPlaylistSongsBinding
+import fullstack.application.spherebeat.databinding.FragmentPostBinding
+import fullstack.application.spherebeat.databinding.FragmentSearchBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,23 +26,28 @@ private const val ARG_PARAM2 = "param2"
  */
 class SearchFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        val searchBar: EditText = binding.searchSearchBar
+        searchBar.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                //filterList(s.toString())
+                Log.v("Search", "Search text changed: " + s.toString())
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+        return binding.root
     }
 
     companion object {
