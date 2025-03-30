e382de8938e85753f8162513e5db9569ecab2671
Gil <gilgil.abramovich@gmail.com>
2025-03-30 04:12:12 +0300

    Cleanups

--- app/src/main/java/fullstack/application/spherebeat/ui/EditProfileFragment.kt
+++ app/src/main/java/fullstack/application/spherebeat/ui/EditProfileFragment.kt
@@ -5,14 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import fullstack.application.spherebeat.R
-import fullstack.application.spherebeat.databinding.FragmentCreatePlaylistBinding
 import fullstack.application.spherebeat.databinding.FragmentEditProfileBinding
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -31,25 +24,5 @@ class EditProfileFragment : Fragment() {
         val args = EditProfileFragmentArgs.fromBundle(requireArguments())
         binding.username.setText(args.userName)
         return binding.root
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment EditProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            EditProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
